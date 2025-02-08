2cf2a911499699cf153bc70405d5e1bf2a56779f
ash0345 <113393782+ash0345@users.noreply.github.com>
2025-02-07 17:13:16 -1000

    added new wedding repo

--- java/script.js
+++ java/script.js
@@ -1,12 +1,42 @@
+// Women 
 const button = document.getElementById('button');
 const bridesmaidList = document.getElementById('bridesmaids');
-const groomsmenList = document.getElementById('groomsmen');
 let userTextInput = document.getElementById('name-input');
 
-button.addEventListener('click', () => {
+// Bridesmaids functions
+clearText = () => {
+    userTextInput.value = '';
+}
 
+button.addEventListener('click', () => {
     let newPersonListItem = document.createElement('li');
-    // console.log(newPersonListItem);
     newPersonListItem.innerText = userTextInput.value;
+    newPersonListItem.classList.add("name");
     bridesmaidList.appendChild(newPersonListItem);
+    clearText();
 });
+
+// Men
+const button2 = document.getElementById('button2');
+const groomsmenList = document.getElementById('groomsmen');
+let userInputMen = document.getElementById('name-input-men');
+
+// Groomsmen function
+clearTextMen = () => {
+    userInputMen.value = '';
+}
+
+button2.addEventListener('click', () => {
+    let newGroomsmenListItem = document.createElement('li');
+    newGroomsmenListItem.innerText = userInputMen.value;
+    newGroomsmenListItem.classList.add('name');
+    groomsmenList.appendChild(newGroomsmenListItem);
+    clearTextMen();
+});
+
+// Removing Items ----- need help with how to get remove to work!!
+const names = document.querySelectorAll('.name');
+
+names.forEach(name => name.addEventListener('click', (event) => {
+    name.remove();
+}))
